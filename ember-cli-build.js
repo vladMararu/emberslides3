2593137ced7db90308bfd3fe418600f338a57aeb
calin.ortan <calin.ortan@softvision.ro>
2016-04-28 10:17:46 +0300

    Project Setup

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,10 +1,12 @@
 /*jshint node:true*/
 /* global require, module */
-var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
-  var app = new EmberApp(defaults, {
-    // Add options here
+  const app = new EmberApp(defaults, {
+    sassOptions: {
+      includePaths: ['app'],
+    }, // Add options here
   });
 
   // Use `app.import` to add additional libraries to the generated
