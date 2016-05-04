0903dcd73e289fcf23b8aab36720a3c529dd5945
calin.ortan <calin.ortan@softvision.ro>
2016-05-04 12:25:54 +0300

    feat canvas-toolbar
    add canvas toolbar component
    add text align component
    add components for adding elements
    add comp for a color picker in the canvas toolbar
    add tests for these comps
    edit: added tests for canvas toolbar panel and toolbar color picker
    edit: implemented one add element component instead of having 3
     	components doing the same thing

--- app/application/controller.js
+++ app/application/controller.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    handleAddLine() {
+      console.log('controller Line');
+    },
+    handleAddText() {
+      console.log('controller Text');
+    },
+    handleAddImg() {
+      console.log('controller Img');
+    },
+    handleTextAlign(param) {
+      console.log('controller' + param);
+    },
+    handleColorSelect(param) {
+      console.log('controller' + param);
+    },
+  },
 });
