9c99c1e382abc6c2d75455534ba802ebad9d5a15
Nicu Campian <nicu.campian@softvision.ro>
2016-05-16 10:32:51 +0300

    temporar commit for all slides component

--- app/components/slide-list-component/component.js
+++ app/components/slide-list-component/component.js
@@ -1,5 +1,36 @@
 import Ember from 'ember';
 
+let slidesHolder = [];
+
 export default Ember.Component.extend({
   classNames: ['slide-list'],
+  slides: null,
+  init() {
+    this._super(...arguments);
+    this.set('slides', slidesHolder);
+  },
+  dragulaconfig: {
+    options: {
+      copy: false,
+      revertOnSpill: false,
+      removeOnSpill: false,
+        // Other options from the dragula source page.
+    },
+    enabledEvents: ['drag', 'drop'],
+  },
+  actions: {
+    addEmptySlide() {
+      const d = new Date();
+      const emptySlide = Ember.Object.extend().create({ id: d.valueOf(), shapes: [] });
+      this.get('slides').pushObject(emptySlide);
+      console.log(this.get('slides'));
+    },
+    onClickRemove(obj) {
+      this.set('slides', this.get('slides').without(obj));
+    },
+    onDrag() {
+    },
+    onDrop(el, target, source, sibling) {
+    },
+  },
 });
